4adadfd0b10647d607cac6892a6eb67ccf5c567b
Alexis Lenoir <alexis.lenoir@sightcall.com>
2022-11-02 22:09:19 +0100

    Correct some missing assets

--- src/sections/Game.jsx
+++ src/sections/Game.jsx
@@ -26,12 +26,12 @@ export default function Game() {
         <div>
           <h2 className="text-center">{t("gameTitle")}</h2>
           <div className="text-center score d-flex">
-            <div class="mr-2">
+            <div className="mr-2">
               {t("gameAlexisHighscore")} :<span className="ml-2">95</span>
             </div>
           </div>
           <div className="text-center score d-flex">
-            <div class="mr-2">
+            <div className="mr-2">
               {t("gameCurrentScore")} :
               <span id="score" className="ml-2">
                 0
@@ -39,7 +39,7 @@ export default function Game() {
             </div>
           </div>
           <div className="text-center highscore d-flex">
-            <div class="mr-2">
+            <div className="mr-2">
               {t("gameHighScore")} :
               <span id="highscore" className="ml-2">
                 0
